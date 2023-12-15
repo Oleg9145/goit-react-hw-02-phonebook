3bb544110d04aa46347e1b1a15817c624206f1ed
Oleg9145 <oleg.zinenko94@gmail.com>
2023-12-15 17:02:01 +0100

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import {Feedback} from './Feedback.jsx'; 
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Feedback />
+       
+      </div>
+    );
+  }
+}
